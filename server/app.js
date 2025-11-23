9ae1c82f8941d4dd5ae6bed87e6fa0e7f71d3f27
ChaitanyaMardane <chaitanyamardane2003@gamil.com>
2025-11-23 23:51:54 +0530

    update cors policy

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "https://homestay-frontend-5xcp.onrender.com/", // frontend origin
+    origin: "https://homestay-frontend-5xcp.onrender.com", // frontend origin
     credentials: true, // allow cookies / credentials
   })
 );
