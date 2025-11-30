f3746d3c3d150ea7d0c5812ca9786355ccc80c11
ChaitanyaMardane <142397032+ChaitanyaMardane@users.noreply.github.com>
2025-11-30 06:59:57 +0530

    Add CORS origin for frontend application

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 app.use(
 cors({
+    origin: 'https://homestay-frontend-5xcp.onrender.com/'
     credentials: true, // allow cookies / credentials
   })
 );
